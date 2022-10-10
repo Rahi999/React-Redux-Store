1f0d72c3c89130ecfda97c39c3749d127bf2d03c
Rahim Ansari <101566073+Rahi999@users.noreply.github.com>
2022-10-10 06:53:05 +0530

    Initial commit

---
+++ src/Redux/AppReducer/reducer.js
@@ -0,0 +1,7 @@
+const initState = {};
+export const reducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./Redux/store";
+// console.log(store.getState());
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
